fce4de114547ac062146c61748b60c193dc10e8b
Bakytbek314 <spidybats72@gmail.com>
2025-03-21 11:44:41 +0600

    predeploy3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "abaplaneta-backend.onrender.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
