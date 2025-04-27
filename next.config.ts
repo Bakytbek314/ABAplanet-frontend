eb2d09ba7a049dc9f5a71d4d2c9cd28d00a5984e
Bakytbek314 <spidybats72@gmail.com>
2025-04-27 21:15:44 +0600

    fix_configv

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
-        port: '5000',
+        port: "5000",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "abaplaneta-backend.onrender.com",
-        pathname: "/**"
-      }
+        pathname: "/**",
+      },
     ],
   },
 };
